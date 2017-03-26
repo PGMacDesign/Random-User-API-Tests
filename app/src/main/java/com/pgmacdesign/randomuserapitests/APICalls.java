71f25abb253b7d8a5121cf3c88effe160a6e9a9a
Patrick MacDowell <pgmacdesign@gmail.com>
2017-03-25 21:19:03 -0700

    API Calls and netowrking class finished

--- app/src/main/java/com/pgmacdesign/randomuserapitests/APICalls.java
+++ app/src/main/java/com/pgmacdesign/randomuserapitests/APICalls.java
@@ -1,8 +1,115 @@
 package com.pgmacdesign.randomuserapitests;
 
+import android.content.Context;
+
+import com.pgmacdesign.pgmacutilities.adaptersandlisteners.OnTaskCompleteListener;
+import com.pgmacdesign.pgmacutilities.networkclasses.retrofitutilities.RetrofitClient;
+import com.pgmacdesign.pgmacutilities.nonutilities.PGMacUtilitiesConstants;
+import com.pgmacdesign.pgmacutilities.utilities.NetworkUtilities;
+
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 /**
  * Class for API Calls. Will likely only be one, but still modularizing it regardless
  * Created by PatrickSSD2 on 3/25/2017.
  */
 public class APICalls {
+
+    private static NetworkInterface client;
+    private static Context context;
+    /**
+     * Static builder for hte interface client.
+     */
+    private static void init() {
+        MyApplication.getInstance();
+        if(client == null){
+            RetrofitClient.Builder builder = new RetrofitClient.Builder(
+                    NetworkInterface.class, Constants.URL_BASE);
+            builder.setDateFormat(Constants.DATE_FORMAT);
+            if(BuildConfig.DEBUG) {
+                builder.setLogLevel(HttpLoggingInterceptor.Level.BODY);
+            } else {
+                builder.setLogLevel(HttpLoggingInterceptor.Level.NONE);
+            }
+            builder.setTimeouts(
+                    (int) PGMacUtilitiesConstants.ONE_MINUTE,
+                    (int)PGMacUtilitiesConstants.ONE_MINUTE);
+            RetrofitClient client1 = builder.build();
+            client = client1.buildServiceClient();
+        }
+        if(context == null){
+            context = MyApplication.getContext();
+        }
+    }
+
+    /**
+     * Simple check for internet connection. If it is not avail, the call will not begin
+     * {@link NetworkUtilities}
+     * @return Boolean, true if internet is connected and call can be made.
+     */
+    private static boolean canProceed(){
+        try {
+            return NetworkUtilities.haveNetworkConnection(MyApplication.getContext());
+        } catch (Exception e){
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    /**
+     * API Call to retrieve the users from the RandomUser web call
+     * @param listener Listener to pass data back on.
+     *                 {@link OnTaskCompleteListener}
+     * @param pageNumber The page number to view. Used in pagination
+     */
+    public static void getUsers(final OnTaskCompleteListener listener, Integer pageNumber){
+        if(listener == null){
+            return;
+        }
+        init();
+        if(!canProceed()){
+            listener.onTaskComplete(Constants.STRING_NO_CONNECTIVITY,
+                    Constants.TAG_NO_NETWORK_CONNECTION);
+            return;
+        }
+        if(pageNumber == null){
+            pageNumber = 0;
+        }
+        Call<RandomUserPojo> call = client.getUsers(Constants.API_CALL_SEED,
+                Constants.NUM_RESULTS_INT, pageNumber, null, null, null, null);
+        call.enqueue(new Callback<RandomUserPojo>() {
+            @Override
+            public void onResponse(Call<RandomUserPojo> call, Response<RandomUserPojo> response) {
+
+                String error = Constants.STRING_UNKNOWN_ERROR;
+                if(response.isSuccessful()){
+                    RandomUserPojo pojo = (RandomUserPojo) response.body();
+                    if(pojo != null) {
+                        listener.onTaskComplete(pojo, Constants.TAG_RETROFIT_CALL_SUCCESS);
+                    } else {
+                        listener.onTaskComplete(error, Constants.TAG_RETROFIT_CALL_FAILURE);
+                    }
+                } else {
+                    //Call was not successful
+                    try {
+                        error = response.errorBody().string();
+                    } catch (Exception e){
+                        e.printStackTrace();
+                    }
+                    listener.onTaskComplete(error, Constants.TAG_RETROFIT_CALL_FAILURE);
+                }
+            }
+
+            @Override
+            public void onFailure(Call<RandomUserPojo> call, Throwable throwable) {
+                throwable.printStackTrace();
+                listener.onTaskComplete(throwable.getMessage(),
+                        Constants.TAG_RETROFIT_CALL_FAILURE);
+            }
+        });
+    }
 }
+
--- app/src/main/java/com/pgmacdesign/randomuserapitests/NetworkInterface.java
+++ app/src/main/java/com/pgmacdesign/randomuserapitests/NetworkInterface.java
@@ -1,8 +1,39 @@
 package com.pgmacdesign.randomuserapitests;
 
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
 /**
  * Network interface for retrofit calls
  * Created by PatrickSSD2 on 3/25/2017.
  */
 public interface NetworkInterface {
+
+    static final String API_BASE = "/api";
+
+    /**
+     * Get Users from the API
+     * @param seed Custom seed. For this project I will be using one custom, unchanging seed
+     * @param numResults Int, number of results to return
+     * @param numPage Int, which page number to be on (For pagination)
+     * @param gender String gender, to return only people of that gender
+     * @param customFormat String, custom return format, defaults to json, but can do xml + more
+     * @param commaDelFieldsToExclude String, comma-separated string of fields to
+     *                                exclude (IE: gender, name, nat) etc.
+     * @param makeDownloadable Send in the String dl if you want the return to be downloadable.
+     *                         This is useful for parsing and storing locally.
+     * @return {@link RandomUserPojo}
+     */
+    @GET(API_BASE)
+    Call<RandomUserPojo> getUsers(@Query("seed") String seed,
+                                  @Query("results") int numResults,
+                                  @Query("page") int numPage,
+                                  @Query("gender") String gender,
+                                  @Query("format") String customFormat,
+                                  @Query("exc") String commaDelFieldsToExclude,
+                                  @Query("dl") String makeDownloadable
+    );
+
+
 }
